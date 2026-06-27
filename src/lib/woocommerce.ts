6bc64b7186eaed37c05e90adbd9b4cf512e01768
Armando <armando.ovalle@gmail.com>
2026-06-26 21:18:52 -0500

    Forzar queryStringAuth en WooCommerce para evitar bloqueos de Netlify

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -4,5 +4,6 @@ export const api = new WooCommerceRestApi({
   url: process.env.NEXT_PUBLIC_WOOCOMMERCE_URL || "",
   consumerKey: process.env.WOOCOMMERCE_KEY || "",
   consumerSecret: process.env.WOOCOMMERCE_SECRET || "",
-  version: "wc/v3"
+  version: "wc/v3",
+  queryStringAuth: true
 });
